58b6b6eb35d476fece6f7d417e9b0967ca9cc73c
mjlee111Ubuntu <menggu1234@naver.com>
2023-10-30 06:15:09 +0900

    update

--- pdf.py
+++ pdf.py
@@ -3,6 +3,7 @@ import fitz  # PyMuPDF
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
+print("\033[92m PDF File Similarity Tester \033[0m")
 
 # PDF 파일을 텍스트로 추출
 def extract_text_from_pdf(pdf_path):
@@ -31,20 +32,18 @@ similarities = []
 for i in range(len(pdf_files)):
     for j in range(i + 1, len(pdf_files)):
         similarity = cosine_similarities[i][j] * 100  # 백분율로 표시
-        similarity = round(similarity, 2)  # 소수점 이하 두 자리까지 표시
-        pair = (pdf_files[i], pdf_files[j], similarity)
+        similarity = round(similarity, 4)  # 소수점 이하 두 자리까지 표시
+        stringsimilarity = "\033[91m" + str(similarity) + "\033[0m"
+        pair = (pdf_files[i], pdf_files[j], stringsimilarity)
         similarities.append(pair)
 
 # 유사도가 높은 순으로 정렬
 similarities.sort(key=lambda x: x[2], reverse=True)
 
-# pprint로 출력
-import pprint
 RED = "\033[91m"
 GREEN = "\033[92m"
 RESET = "\033[0m"
 
-
-pprint.pprint(["이름1        이름2        유사도"],indent=2, width=35)
+print("\033[92m 이름1         이름2         유사도\033[0m")
 for pair in similarities:
-    pprint.pprint(pair, indent=2, width=60)
+    print(f"{pair[0]}     {pair[1]}     {pair[2]}")
